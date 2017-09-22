48fed7c3b3650d5f0888ba9f0736723b63b2cef1
Michael Pu <michael.pu123@gmail.com>
2017-09-22 10:56:10 -0400

    made graph class

--- DMOPC/src/DMOPC14c2p5.java
+++ DMOPC/src/DMOPC14c2p5.java
@@ -3,36 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 import java.io.BufferedReader;
-
-class DirectedGraph<T> implements Iterable<T> {
-
-    private final Map<T, ArrayList<T>> mGraph = new HashMap<>();
-
-    public boolean addNode(T node) {
-        if (mGraph.containsKey(node))
-            return false;
-
-        mGraph.put(node, new ArrayList<T>());
-        return true;
-    }
-
-    public void addEdge(T start, T dest) {
-        mGraph.get(start).add(dest);
-    }
-
-    public ArrayList<T> edgesFrom(T node) {
-        ArrayList<T> arcs = mGraph.get(node);
-        return arcs;
-    }
-
-    public Iterator<T> iterator() {
-        return mGraph.keySet().iterator();
-    }
-}
-
 public class DMOPC14c2p5 {
 
-    private static DirectedGraph<Integer> graph = new DirectedGraph<>();
+    private static Map<Integer, ArrayList<Integer>> map = new HashMap<>();
     private static double[] probList;
 
     public static void main (String[] args) throws IOException {
@@ -44,14 +17,14 @@ public class DMOPC14c2p5 {
         probList = new double[numLake+1];
 
         for (int i=0; i<=numLake; i++) {
-            graph.addNode(i);
+        	map.put(i, new ArrayList<Integer>());
         }
 
         for (int i=0; i<numRiver; i++) {
             tokens = in.readLine().split(" ");
             int start = Integer.parseInt(tokens[0]);
             int end = Integer.parseInt(tokens[1]);
-            graph.addEdge(start, end);
+            map.get(start).add(end);
         }
 
         getProb(1, 1.0d);
@@ -59,7 +32,7 @@ public class DMOPC14c2p5 {
         for (int i=1; i<probList.length; i++) {
             if (probList[i] != -1) {
                 //System.out.println(graph.edgesFrom(i));
-                if (graph.edgesFrom(i).size() == 0) {
+                if (map.get(i).size() == 0) {
                     System.out.println(probList[i]);
                 }
             }
@@ -67,7 +40,7 @@ public class DMOPC14c2p5 {
     }
 
     private static void getProb(int node, double prob) {
-        ArrayList<Integer> curNode = graph.edgesFrom(node);
+        ArrayList<Integer> curNode = map.get(node);
         //System.out.println("Exploring: " + node);
         //System.out.println("Current Node: " + curNode);
         //System.out.println("Probability: " + prob);
