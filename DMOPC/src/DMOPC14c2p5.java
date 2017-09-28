c471d58aacab73e11762a423e4926a488f54ac73
Michael Pu <michael.pu123@gmail.com>
2017-09-27 20:22:29 -0400

    battle positions

--- DMOPC/src/DMOPC14c2p5.java
+++ DMOPC/src/DMOPC14c2p5.java
@@ -1,60 +1,103 @@
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 import java.io.BufferedReader;
 public class DMOPC14c2p5 {
 
-    private static Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-    private static double[] probList;
+    private static ArrayList<Integer>[] map = new ArrayList[1000001];
+    private static double[] dp;
 
-    public static void main (String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String[] tokens = in.readLine().split(" ");
-        int numLake = Integer.parseInt(tokens[0]);
-        int numRiver = Integer.parseInt(tokens[1]);
+    static class FastReader
+    {
+        BufferedReader br;
+        StringTokenizer st;
 
-        probList = new double[numLake+1];
-
-        for (int i=0; i<=numLake; i++) {
-        	map.put(i, new ArrayList<Integer>());
+        public FastReader()
+        {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
         }
 
-        for (int i=0; i<numRiver; i++) {
-            tokens = in.readLine().split(" ");
-            int start = Integer.parseInt(tokens[0]);
-            int end = Integer.parseInt(tokens[1]);
-            map.get(start).add(end);
-        }
-
-        getProb(1, 1.0d);
-
-        for (int i=1; i<probList.length; i++) {
-            if (probList[i] != -1) {
-                //System.out.println(graph.edgesFrom(i));
-                if (map.get(i).size() == 0) {
-                    System.out.println(probList[i]);
+        String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
                 }
             }
+            return st.nextToken();
+        }
+
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 
-    private static void getProb(int node, double prob) {
-        ArrayList<Integer> curNode = map.get(node);
-        //System.out.println("Exploring: " + node);
-        //System.out.println("Current Node: " + curNode);
-        //System.out.println("Probability: " + prob);
-        int size = curNode.size();
-        //System.out.println("Size: " + size);
-        if (size == 0) {
-            //System.out.println("Added final prob " + prob);
-            probList[node] += prob;
-            //System.out.println("Updated node: " + probList[node]);
-        } else {
-            for (int i=0; i<size; i++) {
-                probList[node] = -1;
-                //System.out.println("Exploring prob " + (((double)(1)/(double)(size))*prob) + " at location " + curNode.get(i));
-                getProb(curNode.get(i), (((double)(1)/(double)(size))*prob));
+    public static void main(String[] args) throws IOException {
+        FastReader in = new FastReader();
+        //BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        //String[] tokens = in.readLine().split(" ");
+        //int numLake = Integer.parseInt(tokens[0]);
+        //int numRiver = Integer.parseInt(tokens[1]);
+        int numLake = in.nextInt();
+        int numRiver = in.nextInt();
+
+        dp = new double[numLake + 1];
+
+        for (int i=0; i<=numLake; i++) {
+            map[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < numRiver; i++) {
+            //tokens = in.readLine().split(" ");
+            //map[Integer.parseInt(tokens[0])].add(Integer.parseInt(tokens[1]));
+            map[in.nextInt()].add(in.nextInt());
+        }
+
+        dp[1] = 1.0d;
+        double curDp;
+        for (int i=1; i<=numLake; i++) {
+            int size = map[i].size();
+            if (!(size == 0)) {
+                curDp = dp[i];
+                ArrayList<Integer> destDp = map[i];
+                for (int j=0; j<destDp.size(); j++) {
+                    dp[destDp.get(j)] += (1/(double)size)*curDp;
+                }
+                dp[i] = -1.0d;
+            } else {
+                System.out.println(dp[i]);
             }
         }
     }
